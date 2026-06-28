90fc07d5433d1c0b1bff0e911e6116fcac36ffff
Sajid <sajid@premiumtouchbd.com>
2026-06-29 02:49:17 +0600

    fix: bypass DB queries during console command runs

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -36,56 +36,58 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-       $shurjopay = PaymentGateway::where(['status' => 1, 'type' => 'shurjopay'])->first();
-        if ($shurjopay) {
+        if (!app()->runningInConsole()) {
+            $shurjopay = PaymentGateway::where(['status' => 1, 'type' => 'shurjopay'])->first();
+            if ($shurjopay) {
+                
+                Config::set(['shurjopay.apiCredentials.username' => $shurjopay->username]);
+                Config::set(['shurjopay.apiCredentials.password' => $shurjopay->password]);
+                Config::set(['shurjopay.apiCredentials.prefix' => $shurjopay->prefix]);
+                Config::set(['shurjopay.apiCredentials.return_url' => $shurjopay->success_url]);
+                Config::set(['shurjopay.apiCredentials.cancel_url' => $shurjopay->return_url]);
+                Config::set(['shurjopay.apiCredentials.base_url' => $shurjopay->base_url]);
+            }
+            $generalsetting = GeneralSetting::where('status',1)->limit(1)->first();
+            view()->share('generalsetting',$generalsetting); 
+
+            $sidecategories = Category::where('parent_id','=','0')->where('status',1)->select('id','name','slug','status','image')->get();
+            view()->share('sidecategories',$sidecategories); 
             
-            Config::set(['shurjopay.apiCredentials.username' => $shurjopay->username]);
-            Config::set(['shurjopay.apiCredentials.password' => $shurjopay->password]);
-            Config::set(['shurjopay.apiCredentials.prefix' => $shurjopay->prefix]);
-            Config::set(['shurjopay.apiCredentials.return_url' => $shurjopay->success_url]);
-            Config::set(['shurjopay.apiCredentials.cancel_url' => $shurjopay->return_url]);
-            Config::set(['shurjopay.apiCredentials.base_url' => $shurjopay->base_url]);
+            $menucategories = Category::where('status',1)->select('id','name','slug','status','image')->get();
+            view()->share('menucategories',$menucategories); 
+
+            $contact = Contact::where('status',1)->first();
+            view()->share('contact',$contact); 
+
+            $socialicons = SocialMedia::where('status',1)->get();
+            view()->share('socialicons',$socialicons);
+
+            $pages = CreatePage::where('status',1)->limit(3)->get();
+            view()->share('pages',$pages);
+
+            $pagesright = CreatePage::where('status',1)->skip(1)->limit(5)->get();
+            view()->share('pagesright',$pagesright);
+
+            $cmnmenu = CreatePage::where('status',1)->get();
+            view()->share('cmnmenu',$cmnmenu);
+
+            $brands = Brand::where('status',1)->get();
+            view()->share('brands',$brands);
+            
+            $neworder = Order::where('order_status','1')->count();
+            view()->share('neworder',$neworder); 
+            
+            $pendingorder = Order::where('order_status','1')->latest()->limit(9)->get();
+            view()->share('pendingorder',$pendingorder); 
+            
+            $orderstatus = OrderStatus::get();
+            view()->share('orderstatus',$orderstatus);
+            
+            $pixels = EcomPixel::where('status',1)->get();
+            view()->share('pixels',$pixels);
+            
+            $gtm_code = GoogleTagManager::where('status',1)->get();
+            view()->share('gtm_code',$gtm_code);
         }
-        $generalsetting = GeneralSetting::where('status',1)->limit(1)->first();
-        view()->share('generalsetting',$generalsetting); 
-
-        $sidecategories = Category::where('parent_id','=','0')->where('status',1)->select('id','name','slug','status','image')->get();
-        view()->share('sidecategories',$sidecategories); 
-        
-        $menucategories = Category::where('status',1)->select('id','name','slug','status','image')->get();
-        view()->share('menucategories',$menucategories); 
-
-        $contact = Contact::where('status',1)->first();
-        view()->share('contact',$contact); 
-
-        $socialicons = SocialMedia::where('status',1)->get();
-        view()->share('socialicons',$socialicons);
-
-        $pages = CreatePage::where('status',1)->limit(3)->get();
-        view()->share('pages',$pages);
-
-        $pagesright = CreatePage::where('status',1)->skip(1)->limit(5)->get();
-        view()->share('pagesright',$pagesright);
-
-        $cmnmenu = CreatePage::where('status',1)->get();
-        view()->share('cmnmenu',$cmnmenu);
-
-        $brands = Brand::where('status',1)->get();
-        view()->share('brands',$brands);
-        
-        $neworder = Order::where('order_status','1')->count();
-        view()->share('neworder',$neworder); 
-        
-        $pendingorder = Order::where('order_status','1')->latest()->limit(9)->get();
-        view()->share('pendingorder',$pendingorder); 
-        
-        $orderstatus = OrderStatus::get();
-        view()->share('orderstatus',$orderstatus);
-        
-        $pixels = EcomPixel::where('status',1)->get();
-        view()->share('pixels',$pixels);
-        
-        $gtm_code = GoogleTagManager::where('status',1)->get();
-        view()->share('gtm_code',$gtm_code);
     }
 }
